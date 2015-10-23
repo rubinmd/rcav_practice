06baacdf785767c850c61eeb53b578ecc4c86368
rubinmd <matt@Matthews-MacBook-Pro.local>
2015-10-23 13:34:49 -0500

    Got it working
    Basic functionality. The APR integers are messed up if the user submits
    the wrong number of digits though. It works for 2 digits now.

--- app/controllers/calculations_controller.rb
+++ app/controllers/calculations_controller.rb
@@ -2,4 +2,23 @@ class CalculationsController < ApplicationController
   def instructions
     render("instructions.html.erb")
   end
+
+  def sqrt
+    @the_number=params[:num].to_f
+  end
+
+  def rand
+    @high_num=params[:high_num]
+    @low_num=params[:low_num]
+  end
+
+  def payment
+    @apr=params[:apr].to_f
+    @years=params[:years].to_f
+    @principal=params[:principal].to_f
+    @apr_percent_mon=@apr/1000/12
+    @nper=@years*12
+    @monthly_payment= (@apr_percent_mon * @principal)/(1-(1+@apr_percent_mon)**-@nper)
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
   get("/", { :controller => "calculations", :action => "instructions" })
+  get("/square_root/:num", { :controller => "calculations", :action => "sqrt" })
+  get("/random/:low_num/:high_num", { :controller => "calculations", :action => "rand" })
+  get("/payment/:apr/:years/:principal", { :controller => "calculations", :action => "payment" })
 end
